2109bf2d312d7ccdf6b5ff288f6d2d475758104d
Rx Chris Ameh <chris1jnr@gmail.com>
2025-09-21 07:50:14 +0000

    updated files

---
+++ api/config.js
@@ -0,0 +1,3 @@
+// API configuration
+export const API_KEY = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+export const BASE_URL = "https://api.openweathermap.org/data/2.5";
